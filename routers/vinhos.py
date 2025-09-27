ded78b0ea33bf9bd006921dea7aa21fa37b45812
Nahuel Ayala <NahuelAyala055@gmail.com>
2025-09-27 00:43:10 -0300

    prefix vinhos ajustado

--- routers/vinhos.py
+++ routers/vinhos.py
@@ -7,7 +7,7 @@ from database import SessionLocal, engine
 # cria as tabelas (uma vez)
 models.Base.metadata.create_all(bind=engine)
 
-router = APIRouter(prefix="/vinhos", tags=["vinhos"])
+router = APIRouter(tags=["vinhos"])
 
 # Dependência para obter sessão do banco
 def get_db():
@@ -19,7 +19,7 @@ def get_db():
 
 
 # ------------------ POST ------------------
-@router.post("/", response_model=schemas.VinhoResponse)
+@router.post("/vinhos", response_model=schemas.VinhoResponse)
 def criar_vinho(vinho: schemas.VinhoCreate, db: Session = Depends(get_db)):
     db_vinho = models.Vinho(
         nome=vinho.nome,
@@ -27,20 +27,21 @@ def criar_vinho(vinho: schemas.VinhoCreate, db: Session = Depends(get_db)):
         preco=vinho.preco,
         estoque=vinho.estoque,
         descricao=vinho.descricao,
-        imagem=vinho.imagem  # <-- adiciona aqui
+        imagem=vinho.imagem
     )
     db.add(db_vinho)
     db.commit()
     db.refresh(db_vinho)
     return db_vinho
 
+
 # ------------------ GET ------------------
-@router.get("/", response_model=list[schemas.VinhoResponse])
+@router.get("/vinhos", response_model=list[schemas.VinhoResponse])
 def listar_vinhos(db: Session = Depends(get_db)):
     return db.query(models.Vinho).all()
 
 
-@router.get("/{vinho_id}", response_model=schemas.VinhoResponse)
+@router.get("/vinhos/{vinho_id}", response_model=schemas.VinhoResponse)
 def obter_vinho(vinho_id: int, db: Session = Depends(get_db)):
     vinho = db.query(models.Vinho).filter(models.Vinho.id == vinho_id).first()
     if not vinho:
@@ -49,7 +50,7 @@ def obter_vinho(vinho_id: int, db: Session = Depends(get_db)):
 
 
 # ------------------ PUT ------------------
-@router.put("/{vinho_id}", response_model=schemas.VinhoResponse)
+@router.put("/vinhos/{vinho_id}", response_model=schemas.VinhoResponse)
 def atualizar_vinho(vinho_id: int, vinho: schemas.VinhoCreate, db: Session = Depends(get_db)):
     db_vinho = db.query(models.Vinho).filter(models.Vinho.id == vinho_id).first()
     if not db_vinho:
@@ -60,14 +61,15 @@ def atualizar_vinho(vinho_id: int, vinho: schemas.VinhoCreate, db: Session = Dep
     db_vinho.preco = vinho.preco
     db_vinho.estoque = vinho.estoque
     db_vinho.descricao = vinho.descricao
-    db_vinho.imagem = vinho.imagem  # <-- adiciona aqui
+    db_vinho.imagem = vinho.imagem
     
     db.commit()
     db.refresh(db_vinho)
     return db_vinho
 
+
 # ------------------ DELETE ------------------
-@router.delete("/{vinho_id}")
+@router.delete("/vinhos/{vinho_id}")
 def deletar_vinho(vinho_id: int, db: Session = Depends(get_db)):
     db_vinho = db.query(models.Vinho).filter(models.Vinho.id == vinho_id).first()
     if not db_vinho:
